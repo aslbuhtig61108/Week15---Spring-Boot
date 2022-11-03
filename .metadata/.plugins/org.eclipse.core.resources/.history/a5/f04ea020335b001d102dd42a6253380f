9a4ac9f8ae6d1eb178cd2cb01768623d206ca239
Ed Locquiao <nilatian.15@gmail.com>
2022-11-02 21:57:12 -0700

    W16: Coding Assignment - Everything up to Step 10 before replacing JeepOrderDao and DefaultJeepOrderDao files. Just in case this doesn't work

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/a5/f04ea020335b001d102dd42a6253380f
@@ -0,0 +1,9 @@
+package com.promineotech.jeep.dao;
+
+import com.promineotech.jeep.entity.Customer;
+
+public interface JeepOrderDao {
+
+  Customer fetchCustomer(String customer);
+
+}
---
+++ jeep-sales/src/main/java/com/promineotech/jeep/controller/JeepOrderController.java
@@ -0,0 +1,76 @@
+package com.promineotech.jeep.controller;
+
+import javax.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import com.promineotech.jeep.entity.Order;
+import com.promineotech.jeep.entity.OrderRequest;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+// turn validation on at the class level
+@Validated
+
+// add documentation to describe the controller with a title
+// Any uri that has '/jeeps' after the port number will get mapped to this class
+@RequestMapping("/orders")
+
+@OpenAPIDefinition(info = @Info(title = "Jeep Order Service"),
+    servers = {@Server(url = "http://localhost:8080", description = "Local server.")})
+
+public interface JeepOrderController {
+
+
+  // Placing all the OpenAPI annotations inside the interface helps with organization
+  // @formatter:off
+  @Operation(
+      summary = "Create an order for a Jeep",
+      description = "Returns the created Jeep",
+      responses = { 
+          @ApiResponse(
+              responseCode = "201",
+              description = "The created Jeep is returned",
+              content = @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = Order.class))),
+          @ApiResponse(
+              responseCode = "400", 
+              description = "The request parameters are invalid", 
+              content = @Content(mediaType = "application/json" )),
+          @ApiResponse(
+              responseCode = "404", 
+              description = "A Jeep component was not found with the input criteria",
+              content = @Content(mediaType = "application/json" )),
+          @ApiResponse(
+              responseCode = "500", 
+              description = "An unplanned error occurred.", 
+              content = @Content(mediaType = "application/json" ))
+      },
+      
+      parameters = {
+          @Parameter(
+              name = "orderRequest",
+              allowEmptyValue = true, 
+              required = false, 
+              description = "The order as JSON") 
+          }
+  )
+  
+  @PostMapping
+  
+  // Returns a 201 or created
+  @ResponseStatus(code = HttpStatus.CREATED)
+  Order createOrder(@Valid @RequestBody OrderRequest orderRequest);
+      
+  // @formatter:on
+}
